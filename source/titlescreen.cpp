f4af033766eb0e9fa784c09338eab50918417f29
obluda3 <yoshivert992@gmail.com>
2021-12-06 22:08:12 +0100

    Comment the hacks.

--- source/titlescreen.cpp
+++ source/titlescreen.cpp
@@ -7,11 +7,12 @@ kmWrite32(0x8050F474, 1);
 // That way, he can have his goalkeeper uniform
 kmCallDefAsm(0x800E4924) 
 {
-	cmpwi r30, 1
-	bne end
-	li r26, 1 // Raimon 1 Team_def
-	li r4, 0
+	change_uniform:
+		cmpwi r30, 1 //if Shinsuke
+		bne end
+		li r26, 1 // Raimon 1 Team_def
+		li r4, 0 // Load goalkeeper uniform
 	end:
-	stw r26, 0x50(r29) // Original instruction
-	blr	
+		stw r26, 0x50(r29) // Original instruction
+		blr	
 }
