9722df96a752a7f320bc541003288fc26f51352e
Obluda <65013432+obluda3@users.noreply.github.com>
2022-07-04 17:20:02 +0200

    missing users

--- source/moveset_banlist.cpp
+++ source/moveset_banlist.cpp
@@ -22,16 +22,35 @@ static BanMove BanList[] = {
     { -1, -1 }
 };
 
-static BanMove WhiteList[] = 
+static BanMove WhiteListMix[] = 
 {
     { P_12813TEMMA, W_SAIKYOU_ELEVEN_HADOU, false },
-    { P_10249MAHORO, W_ILLUSION_BALL, false },
-    { P_12009DORIRU, W_ILLUSION_BALL, false },
-    { P_1092DOMON, W_KILLER_SLIDE, false },
     { -1, -1 }
 };
 
-bool IsLocked(u32 move, register u32 player, register u32 isMiximax)
+static BanMove WhiteListReg[] = 
+{
+    { P_10249MAHORO, W_ILLUSION_BALL, false },
+    { P_12009DORIRU, W_ILLUSION_BALL, false },
+    { P_1092DOMON, W_KILLER_SLIDE, false },
+    { P_12011FUEI, W_SARGASSO, false },
+    { P_12013FUEI, W_SARGASSO, false },
+    { P_12809FUEI, W_SARGASSO, false },
+    { -1, -1 }
+};
+
+bool IsAllowedRegular(s32 isFound, s32 player, s32 move)
+{
+    if (isFound) return 1;
+    for (BanMove* ban = WhiteListReg; ban->player != -1; ban++)
+    {
+        if (ban->player == player && ban->move == move)
+            return 1;
+    }
+    return 0;
+}
+
+bool IsLockedMiximax(u32 move, register u32 player, register u32 isMiximax)
 {
     asm("mr player, r22");
     asm("mr isMiximax, r24");
@@ -40,13 +59,24 @@ bool IsLocked(u32 move, register u32 player, register u32 isMiximax)
         if (ban->player == player && ban->move == move && (ban->needsMiximax == isMiximax == true))
             return true;
     }
-    for (BanMove* allow = WhiteList; allow->player != -1; allow++)
+    for (BanMove* ban = WhiteListMix; ban->player != -1; ban++)
     {
-        if (allow->player == player && allow->move == move && allow->needsMiximax == isMiximax == true)
-            return false;
+        if (ban->player == player && ban->move == move && (ban->needsMiximax == isMiximax == true))
+            return 1;
     }
     if (WAZIsMiximax(move) && !IsMiximaxPlayer(player, 0)) return true;
     return false;
 }
 
-kmCall(0x800F8D44, IsLocked);
+kmCall(0x800F8D44, IsLockedMiximax);
+kmBranchDefAsm(0x800F8D38, 0x800F8D3C)
+{
+    nofralloc
+    mr r3, r0
+    mr r4, r22
+    mr r5, r26
+    bl IsAllowedRegular
+    mr r0, r3
+    cmpwi r0, 0
+    blr
+}
