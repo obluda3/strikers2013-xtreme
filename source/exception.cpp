d928811efbe4b07e3e99b1190aa7298479411b2d
Obluda <65013432+obluda3@users.noreply.github.com>
2023-04-19 15:27:33 +0200

    exception handler

---
+++ source/exception.cpp
@@ -0,0 +1,25 @@
+#include <kamek.h>
+// shitty exception handler
+
+extern "C" {
+  int* OSGetStackPointer();
+}
+
+void exception(int context) {
+  char message[1024];
+  char tmp[50];
+
+  int* sp = OSGetStackPointer();
+  int cnt = 0;
+  for (int* i = sp; i; i = (int*)*i) {
+    if (i == (int*)-1) break;
+    if (cnt++ >= 8) break;
+    sprintf(tmp, "0x%08x:   0x%08x    0x%08x\n", i, *i, i[1]);
+    strcat(message, tmp);
+  }
+  unsigned int fg = 0xFFFFFFFF, bg = 0;
+
+  OSFatal(&fg, &bg, message);
+}
+
+kmBranch(0x80289DA0, exception);
