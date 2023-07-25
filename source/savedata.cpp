04705a8bd52d6526a40c995b9770356219932f3c
Obluda <65013432+obluda3@users.noreply.github.com>
2023-07-25 16:49:05 +0200

    Add default save data

--- source/savedata.cpp
+++ source/savedata.cpp
@@ -1,5 +1,7 @@
 #include <kamek.h>
+#include <shd_mem.h>
 #include "xtreme.h"
+#include <dvd.h>
 // sys magic number
 kmWrite32(0x80041040, 0x3860002B);
 kmWrite32(0x80041F10, 0x3880002B);
@@ -24,4 +26,33 @@ void patch_save_name() {
   filenames[2652] = new_save_name;
 }
 
+void default_save_data() {
+  char *path = "save.sav";
+
+  int entrynum = DVDConvertPathToEntrynum(path);
+  OSReport("default save change\n");
+  if (entrynum < 0)
+    OSReport("Could not find '%s' \n", path);
+  else {
+    DVDHandle handle;
+    if (!DVDFastOpen(entrynum, &handle))
+      OSReport("ERROR: Failed to open file!\n");
+    else {
+      u32 length = handle.length, roundedLength = (handle.length + 0x1F) & ~0x1F;
+      char* buffer = (char*)MEMAlloc(roundedLength, 32, 3, 31);
+      if (!buffer)
+        OSReport("ERROR: Out of file memory\n");
+      else {
+        DVDReadPrio(&handle, buffer, roundedLength, 0, 2);
+        DVDClose(&handle);
+        memcpy((void*)0x80526100, buffer + 0x2590, 0x68548);
+        memcpy((void*)0x8058E648, buffer + 0x2590, 0x68548);
+        memcpy((char*)0x805F9120 + 0x2590, buffer + 0x2590, 0x68548);
+        MEMFree(buffer);
+      }
+    }
+  }
+}
+
 kmBranch(0x800241C8, patch_save_name);
+kmBranch(0x800410E4, default_save_data);
