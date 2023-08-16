5abeef0527750c5afdc933bee07de7d64e113009
Obluda <65013432+obluda3@users.noreply.github.com>
2023-08-16 12:04:51 +0200

    wrong url.

--- source/xtreme.cpp
+++ source/xtreme.cpp
@@ -294,7 +294,7 @@ URL_Patch wiimmfi_server[] = {
   {0x80507744, "rnk.xtreme13.com:2201"},
   {0x8050780B, "rnk.xtreme13.com:2201"},
   {0x8050782F, "rnk.xtreme13.com:2201"},
-  {0x8050C1F3, "rnk.xtreme13.com:2201"}
+  {0x8050C1F3, "rnk.xtreme13.com:2201/"}
 };
 
 int domain_urls[] = { 0x805033d8, 0x805039e0, 0x805045e0, 0x80505a45,
